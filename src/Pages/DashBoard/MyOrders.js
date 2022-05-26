250326ad89351207b6feb34826b2eaf00478bbee
monirhosenakanda <monirhosenakanda@gmail.com>
2022-05-26 23:51:19 +0600

    change local url

--- src/Pages/DashBoard/MyOrders.js
+++ src/Pages/DashBoard/MyOrders.js
@@ -1,57 +1,55 @@
-import React, { useEffect, useState } from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import auth from '../../firebase.init';
-import { useNavigate } from 'react-router-dom';
-import { signOut } from 'firebase/auth';
+import React, { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "../../firebase.init";
+import { useNavigate } from "react-router-dom";
+import { signOut } from "firebase/auth";
 
 const MyOrders = () => {
+  const [appointments, setAppointments] = useState([]);
+  const [user] = useAuthState(auth);
+  const navigate = useNavigate();
 
-    const [appointments, setAppointments] = useState([]);
-    const [user] = useAuthState(auth);
-    const navigate = useNavigate()
+  useEffect(() => {
+    if (user) {
+      fetch(`https://obscure-lake-52950.herokuapp.com/user=${user.email}`, {
+        method: "GET",
+        headers: {
+          authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+        },
+      })
+        .then((res) => {
+          console.log("res", res);
+          if (res.status === 401 || res.status === 403) {
+            signOut(auth);
+            localStorage.removeItem("accessToken");
+            navigate("/");
+          }
+          return res.json();
+        })
+        .then((data) => {
+          setAppointments(data);
+        });
+    }
+  }, [user]);
 
-    useEffect(() => {
-        if (user) {
-            fetch(`http://localhost:5000/user=${user.email}`, {
-                method: 'GET',
-                headers: {
-                    'authorization': `Bearer ${localStorage.getItem('accessToken')}`
-                }
-            })
-                .then(res => {
-                    console.log('res', res);
-                    if (res.status === 401 || res.status === 403) {
-                        signOut(auth);
-                        localStorage.removeItem('accessToken');
-                        navigate('/');
-                    }
-                    return res.json()
-                })
-                .then(data => {
-
-                    setAppointments(data);
-                });
-        }
-    }, [user])
-
-    return (
-        <div>
-            <h2>My ORder: {appointments.length}</h2>
-            <div class="overflow-x-auto">
-                <table class="table w-full">
-                    <thead>
-                        <tr>
-                            <th></th>
-                            <th>Product Name</th>
-                            <th>Quantity</th>
-                            <th>Price</th>
-                            <th>Product Image</th>
-                        </tr>
-                    </thead>
-                </table>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <h2>My ORder: {appointments.length}</h2>
+      <div class="overflow-x-auto">
+        <table class="table w-full">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Product Name</th>
+              <th>Quantity</th>
+              <th>Price</th>
+              <th>Product Image</th>
+            </tr>
+          </thead>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default MyOrders;
